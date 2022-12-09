3c6c7e54d58787f794ce3f8b69e2c3a5dd58f32c
Marsel Sharipov <marselsharipov@live.ru>
2022-12-09 16:56:30 +0300

    Поправил краш с падением по нажатию на элемент списка

--- app/src/main/java/com/example/employeelist/models/AddDialogFragment.kt
+++ app/src/main/java/com/example/employeelist/models/AddDialogFragment.kt
@@ -1,54 +1,38 @@
 package com.example.employeelist.models
 
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.os.Build
 import android.os.Bundle
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import com.example.employeelist.EmployeeItemAdapter
-import com.example.employeelist.R
 import com.example.employeelist.databinding.AddEmployeeDialogBinding
 
 class AddDialogFragment: DialogFragment() {
 
-    lateinit var data: EmployeeClass
-
     companion object {
-
-        fun newInstance(employee: EmployeeClass): AddDialogFragment {
-           val dialogFragment = AddDialogFragment()
-            dialogFragment.arguments?.putParcelable("EMPLOYEE", employee)
-            return dialogFragment
+        fun newInstance(employee: EmployeeClass) = AddDialogFragment().apply { //TODO: проблема была вот тут
+            arguments = Bundle().apply {
+                putParcelable("EMPLOYEE", employee)
+            }
         }
     }
 
-
-    private var _binding: AddEmployeeDialogBinding? =
-        null // TODO: во фрагментах гугл советуею юзать вот такой подход
+    private var _binding: AddEmployeeDialogBinding? = null // TODO: во фрагментах гугл советуею юзать вот такой подход
     private val binding get() = _binding!!
 
+    lateinit var data: EmployeeClass
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val data = arguments?.getParcelable<EmployeeClass>("EMPLOYEE")
-            ?: throw IllegalArgumentException("Data argument required") // TODO: перехват данных по ключу.
+        data = arguments?.getParcelable("EMPLOYEE") ?: throw IllegalArgumentException("Data argument required") // TODO: перехват данных по ключу.
         // TODO: IllegalArgumentException тут нужен больше для тестирования, например если разные типы данных по одному ключу передаете
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = AddEmployeeDialogBinding.inflate(layoutInflater)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = AddEmployeeDialogBinding.inflate(inflater, container, false)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
         return binding.root
     }
 
